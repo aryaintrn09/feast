6a9ce900d6a6ae624b15ed5b3486be79c14232fd
Arya Ngurah Intaran <aryaintaran09@proton.me>
2023-10-06 23:39:18 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import Home from "./Components/Home"
 
 function App() {
-  return <div className='App'>Hello</div>
+  return <div className='App'>
+    <Home/>
+  </div>
 }
 
 export default App;
